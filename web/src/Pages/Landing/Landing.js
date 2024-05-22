a4d9bfcca960ca421dd954158dfd5fbd65ff3d81
rendizi <rendizi_off@gmail.com>
2024-05-22 08:37:16 +0500

    Update: fixed alerts

--- web/src/Pages/Landing/Landing.js
+++ web/src/Pages/Landing/Landing.js
@@ -7,6 +7,24 @@ function Landing() {
     const [haveAnAccount, setHaveAnAccount] = React.useState(false)
     const [code, setCode] = React.useState(null)
     const [message, setMessage] = React.useState(null)
+    const [isVisible, setIsVisible] = React.useState(false);
+
+    React.useEffect(() => {
+        if (code !== null) {
+            setIsVisible(true);
+            const timer = setTimeout(() => {
+                setIsVisible(false);
+                setCode(null); 
+                setMessage(null); 
+            }, 5000);
+            return () => {
+                clearTimeout(timer);
+                setCode(null); 
+                setMessage(null); 
+            };
+        }
+    }, [code, message]);
+    
 
 
     return (
@@ -36,20 +54,47 @@ function Landing() {
                 </form>
             </dialog>
         </div>
-        {code !== null && code !== 200 ?
-        <div role="alert" className="alert alert-error fixed bottom-0 right-0 mb-4 mr-4 md:w-1/2 w-full z-50">
-            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-            <span>Error! {message === null ? "Something went wrong. Try again later" : message}</span>
-        </div> : code == 200 &&
-         <div role="alert" className="alert alert-success fixed bottom-0 right-0 mb-4 mr-4 md:w-1/2 w-full z-50">
-         <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
-             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-         </svg>
-         <span>Success! You will be redirected soon</span>
-     </div>
-}
+        {isVisible && code !== 200 ? (
+                <div
+                    role="alert"
+                    className="alert alert-error fixed bottom-0 right-0 mb-4 mr-4 md:w-1/2 w-full z-50 animate-fade-in-out"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="stroke-current shrink-0 h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                    </svg>
+                    <span>Error! {message === null ? "Something went wrong. Try again later" : message}</span>
+                </div>
+            ) : code == 200 && (
+                <div
+                    role="alert"
+                    className="alert alert-success fixed bottom-0 right-0 mb-4 mr-4 md:w-1/2 w-full z-50 animate-fade-in-out"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="stroke-current shrink-0 h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                    </svg>
+                    <span>Success! You will be redirected soon</span>
+                </div>
+            )}
     </>
     
     );
